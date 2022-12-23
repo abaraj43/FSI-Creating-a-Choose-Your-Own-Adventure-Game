1db8274378770a2914993561e0070dc6ed87260b
Areli <arelibarajas9@gmail.com>
2022-12-22 21:26:05 -0800

    change5

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ if(firstAnswer === 'left'){
 It scampers off down a small hole, just large enough for you to crawl through. 
 Do you follow it, or continue on your path?`)
 
-} else if(firstAnswer === 'right') {
+if(firstAnswer === 'follow'){
+    let secondAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of 
+        warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, 
+        but you wonder if you should alert the world to this magical safe haven.')
+
+else if(firstAnswer === 'stay'){
+    let secondAnswer = window.prompt('you live happily amongst the cats for the rest of your days.')}    }
+} 
+
+else if(firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
 lead you away from the dragon altogether. Which path do you take?`)
