0bfc51864840e117dcd521c43761a1d969d0d89c
Areli <arelibarajas9@gmail.com>
2022-12-22 22:35:10 -0800

    ihatethis

--- index.js
+++ index.js
@@ -14,22 +14,42 @@ if(secondAnswer === 'follow'){
             let fourthAswer = window.prompt('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless')
         }
         }
- //continue
- else if(secondAnswer === 'continue'){
+//continue
+else if(secondAnswer === 'continue'){
     let thirdAnswer = window.prompt('You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?')
-        if(thirdAnser === 'stairscase'){
-            let fourthAnswer = window.prompt('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.')
+        if(thirdAnswer === 'stairscase'){
+            let fourthAnswer = window.prompt('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.')}
         else if(thirdAnswer === 'ladder'){
-            let fourthAnswer = window.prompt('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.')
+            let fourthAnswer = window.prompt('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home')
         }
         }
  }       
-} 
 
-        else if(firstAnswer === 'right') {
-            let secondAnswer = window.prompt(`You come across a snoring dragon. 
+
+else if(firstAnswer === 'right') {
+    let secondAnswer = window.prompt(`You come across a snoring dragon. 
             On the other side of him, you see a shiny chest of treasure. Another path would 
             lead you away from the dragon altogether. Which path do you take?`)
+//past the dragon
+if(secondAnswer === 'past the dragon'){
+    let thirdAnswer = window.prompt('The dragon wakes up and sits upright. You only have a moment to respond, to stay or run:')
+        if(thirdAnswer === 'stay'){
+            let fourthAnswer = window.prompt('You and the dragon have an uplifting conversation about local politics and become lifelong friends')
+        }
+        else if(thirdAnswer === 'run'){
+            let fourthAnswer = window.prompt('Quickly, you run back to the caves entrance. Sheepish, you return home')
+        }
+}
+//away from the dragon
+else if(secondAnswer === 'away from the dragon'){
+    let thirdAnswer = window.prompt('After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do?')
+        if(thirdAnswer === 'draw it'){
+            let fourthAnswer = window.prompt('You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch')
+        }
+        else if(thirdAnswer === 'pick it'){
+            let fourthAnswer = window.prompt('You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust.')
+        }
+}
 
 }
 
