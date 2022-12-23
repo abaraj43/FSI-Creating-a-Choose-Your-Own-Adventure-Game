b68f968ab547d11ac50132dff278c11f7ac24145
Areli <arelibarajas9@gmail.com>
2022-12-22 22:06:49 -0800

    ihatethis

--- index.js
+++ index.js
@@ -4,19 +4,32 @@ if(firstAnswer === 'left'){
 It scampers off down a small hole, just large enough for you to crawl through. 
 Do you follow it, or continue on your path?`)
 
-if(firstAnswer === 'follow'){
-    let secondAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of 
-        warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, 
-        but you wonder if you should alert the world to this magical safe haven.')
+//follow
+if(secondAnswer === 'follow'){
+    let thirdAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
 
-else if(firstAnswer === 'stay'){
-    let secondAnswer = window.prompt('you live happily amongst the cats for the rest of your days.')}    }
+        if(thirdAnswer === 'stay'){
+            let fourthAnswer = window.prompt('you live happily amongst the cats for the rest of your days.')}    
+        else if(thirdAnswer === 'spread the word'){
+            let fourthAswer = window.prompt('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless')
+        }
+        }
+ //continue
+ else if(secondAnswer === 'continue'){
+    let thirdAnswer = window.prompt('You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?')
+        if(thirdAnser === 'stairscase'){
+            let fourthAnswer = window.prompt('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.')
+        else if(thirdAnswer === 'ladder'){
+            let fourthAnswer = window.prompt('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.')
+        }
+        }
+ }       
 } 
 
-else if(firstAnswer === 'right') {
-    let secondAnswer = window.prompt(`You come across a snoring dragon. 
-On the other side of him, you see a shiny chest of treasure. Another path would 
-lead you away from the dragon altogether. Which path do you take?`)
+        else if(firstAnswer === 'right') {
+            let secondAnswer = window.prompt(`You come across a snoring dragon. 
+            On the other side of him, you see a shiny chest of treasure. Another path would 
+            lead you away from the dragon altogether. Which path do you take?`)
 
 }
 
